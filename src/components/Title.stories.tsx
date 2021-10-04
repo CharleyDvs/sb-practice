6cef23148f5ca9d61b69d6f2989da2708f15df47
Carlos Yerena <cyerena@integralads.com>
2021-10-04 17:31:53 -0500

    Setup Eslint and prettier

---
+++ src/components/Title.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Title } from './Title';
+
+export default {
+    title: 'Components/Title',
+    component: Title
+} as ComponentMeta<typeof Title>;
+
+export const DefaultTitle: ComponentStory<typeof Title> = () => <Title />;
---
+++ src/components/Title.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Title = (): JSX.Element => <h1>Component Example</h1>;
